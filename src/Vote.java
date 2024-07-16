8b083d663acee2a6dd0cff0bdab0a531be34e304
Prinsa <prinsashrestha979@gmail.com>
2024-07-16 17:51:43 +0545

    Voting

---
+++ src/Vote.java
@@ -0,0 +1,11 @@
+public class Vote {
+    public static void main(String[]args){
+        int age = 27;
+        if(age >= 18){
+            System.out.println("The person can vote.");
+        }
+        else{
+            System.out.println("The person cannot vote. ");
+        }
+    }
+}
